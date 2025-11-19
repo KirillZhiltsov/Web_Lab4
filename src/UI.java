2821d9eb826f590573196521af548b7c8772dde2
rpc70 <rpc70917@gmail.com>
2025-11-20 01:24:56 +0300

    Изменения

--- src/UI.java
+++ src/UI.java
@@ -15,10 +15,11 @@ public class UI {
             System.out.println("Выберите команду:");
             System.out.println("1 -> Добавить в очередь числа");
             System.out.println("2 -> Вывести очередь");
-            System.out.println("3 -> Создать файл");
-            System.out.println("4 -> Сохранить очередь в файл");
-            System.out.println("5 -> Вывести содержимое файла");
-            System.out.println("6 -> Удалить файл");
+            System.out.println("3 -> Выполнить сдвиг");
+            System.out.println("4 -> Создать файл");
+            System.out.println("5 -> Сохранить очередь в файл");
+            System.out.println("6 -> Вывести содержимое файла");
+            System.out.println("7 -> Удалить файл");
             System.out.println("0 -> Завершить работу программы");
             System.out.print("Введите номер команды: ");
 
@@ -32,6 +33,7 @@ public class UI {
                 case "4" -> System.out.println("4");
                 case "5" -> System.out.println("5");
                 case "6" -> System.out.println("6");
+                case "7" -> System.out.println("7");
                 case "0" -> {
                     System.out.println("Завершение");
                     return;
