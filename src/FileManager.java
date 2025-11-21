686f3456aacb283cb8168406e6a8c69ec7e3e06d
rpc70 <rpc70917@gmail.com>
2025-11-21 10:56:59 +0300

    The end

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.Queue;
-
 
 /**
  * Класс работы с файлами
--- src/QueueRealNumberIterator.java
+++ src/QueueRealNumberIterator.java
@@ -1,24 +1,44 @@
 import java.util.*;
 
+/**
+ * Класс-итератор для работы с очередью вещественных чисел
+ */
 public class QueueRealNumberIterator implements Iterator<RealNumber> {
+
+    /**
+     * Очередь, содержащая объекты вещественных чисел
+     */
     private final Queue<RealNumber> numbers;
+
+    /**
+     * Внутренний итератор для перебора элементов очереди
+     */
     private final Iterator<RealNumber> internalIterator;
 
     /**
-     * Конструктор с параметрами
-     *
-     * @param numbers Коллекция RealNumber
+     * Конструктор класса QueueRealNumberIterator
+     * @param numbers очередь, содержащая объекты типа RealNumber
+     * @throws NullPointerException если переданная очередь равна null
      */
     public QueueRealNumberIterator(Queue<RealNumber> numbers) {
         this.numbers = numbers;
         this.internalIterator = numbers.iterator();
     }
 
+    /**
+     * Проверяет наличие следующего элемента в очереди
+     * @return true, если есть следующий элемент, иначе false
+     */
     @Override
     public boolean hasNext() {
         return internalIterator.hasNext();
     }
 
+    /**
+     * Возвращает следующий элемент из очереди
+     * @return объект типа RealNumber
+     * @throws NoSuchElementException если элементы в очереди закончились
+     */
     @Override
     public RealNumber next() {
         if (!hasNext()) {
@@ -26,5 +46,4 @@ public class QueueRealNumberIterator implements Iterator<RealNumber> {
         }
         return internalIterator.next();
     }
-
 }
